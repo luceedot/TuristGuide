ed858e1e85995eeaf1c585977557dd66842eb6e3
luceedot <asqv0001@stud.kea.dk>
2025-02-04 14:12:02 +0100

    første commit

---
+++ src/main/java/com/example/turistguideopgave/TuristGuideOpgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.turistguideopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristGuideOpgaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuristGuideOpgaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turistguideopgave/TuristGuideOpgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turistguideopgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGuideOpgaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
